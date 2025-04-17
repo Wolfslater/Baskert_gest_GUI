ab6d6c0854e321fc572445da047c8278aaed13b9
wolfslayer <diegodisante22@gmail.com>
2025-04-17 20:39:50 +0200

    Update gestionaleGUI.py

--- first_GUI_code/gest_cest_GUI - Copia/gestionaleGUI.py
+++ first_GUI_code/gest_cest_GUI - Copia/gestionaleGUI.py
@@ -6,19 +6,17 @@ class gestionaleGUI:
         self.frame = Frame(self.master)
         self.master.geometry("800x600")
         self.master.title("Gestionale GUI cestini di frutta")
+
         self.addBtn = Button(self.master, text="Add fruit to the basket.", command=self.add)
         self.exitBtn = Button(self.master, text="Exit the the program.", command=self.exit)
-
-        self.addBtn.pack()
-        self.exitBtn.pack()
-        self.frame.pack()
+        self.addBtn.grid(row=0, column=0, padx=50, pady=0)
+        self.exitBtn.grid(row=0, column=1, padx=15, pady=0)
 
         self.fruit = Frutto()
 
     def add(self):
-        self.master.withdraw()
         self.newWindow = Toplevel(self.master)
-        new_fruit = addFruit(self.master, self.newWindow, self.fruit)
+        addFruit(self.newWindow, self.master, self.fruit)
 
     def exit(self):
         exit()
