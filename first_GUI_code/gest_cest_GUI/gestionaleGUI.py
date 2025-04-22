af6e90a1e61d0afcb1ef934bd762c89ffd309061
wolfslayer <diegodisante22@gmail.com>
2025-04-22 10:31:43 +0200

    Update gestionaleGUI.py

--- first_GUI_code/gest_cest_GUI/gestionaleGUI.py
+++ first_GUI_code/gest_cest_GUI/gestionaleGUI.py
@@ -1,6 +1,7 @@
 #Version 0.3.3 21/04/2925
 
 from addFrutto import Button, Frutto, addFruit
+from basketManager import BasketInfos
 from tkinter import Toplevel
 from baskets import (basket_1, basket_2, 
                      basket_3, basket_4, basket_5)
@@ -13,11 +14,11 @@ class gestionaleGUI:
 
         self.addBtn = Button(self.master, text="Add fruit to the basket.", command=self.add)
         self.exitBtn = Button(self.master, text="Exit the the program.", command=self.exit)
-        self.print = Button(self.master, text="pritn basket 1", command=self.printing)
+        self.basketBtn = Button(self.master, text="Open basket manager", command=self.basketManager)
 
         self.addBtn.grid(row=0, column=0, padx=50, pady=0)
         self.exitBtn.grid(row=0, column=1, padx=15, pady=0)
-        self.print.grid(row=0, column=2, padx=50, pady=0)
+        self.basketBtn.grid(row=0, column=2, padx=50, pady=0)
 
         self.fruit = Frutto()
 
@@ -28,6 +29,11 @@ class gestionaleGUI:
 
     def exit(self):
         exit()
+
+    def basketManager(self):
+        self.master.withdraw()
+        self.newWindow = Toplevel(self.master)
+        BasketInfos(self.newWindow, self.master)
 
     def printing(self):
         print(basket_1)
