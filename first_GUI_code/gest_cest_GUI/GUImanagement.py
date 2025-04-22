eee9abbe1e93db760da6d6bfe0cba03294bad7e0
wolfslayer <diegodisante22@gmail.com>
2025-04-22 23:00:49 +0200

    Update GUImanagement.py

--- first_GUI_code/gest_cest_GUI/GUImanagement.py
+++ first_GUI_code/gest_cest_GUI/GUImanagement.py
@@ -1,4 +1,4 @@
-#Version 0.3.3 21/04/2925
+# Version 0.3.3 21/04/2925
 
 from addFruit import Button, Frutto, addFruit
 from basketManager import BasketInfos
@@ -7,28 +7,30 @@ from tkinter import Toplevel
 class GUIManagement:
     def __init__(self, master):
         self.master = master
-        self.master.geometry("800x600")
-        self.master.title("Gestionale GUI cestini di frutta")
+        self.master.geometry("800x600")  # Set window size
+        self.master.title("Gestionale GUI cestini di frutta")  # Set title
 
+        # Buttons with commands
         self.addBtn = Button(self.master, text="Add fruit to the basket.", command=self.add)
         self.exitBtn = Button(self.master, text="Exit the the program.", command=self.exit)
         self.basketBtn = Button(self.master, text="Open basket manager", command=self.basketManager)
 
-        self.addBtn.grid(row=0, column=0, padx=50, pady=0)
-        self.exitBtn.grid(row=0, column=1, padx=15, pady=0)
-        self.basketBtn.grid(row=0, column=2, padx=50, pady=0)
+        # Layout
+        self.addBtn.grid(row=0, column=0, padx=50)
+        self.exitBtn.grid(row=0, column=1, padx=15)
+        self.basketBtn.grid(row=0, column=2, padx=50)
 
-        self.fruit = Frutto()
+        self.fruit = Frutto()  # Initialize fruit object
 
-    def add(self):
+    def add(self):  # Open add fruit window
         self.master.withdraw()
         self.newWindow = Toplevel(self.master)
         addFruit(self.newWindow, self.master, self.fruit)
 
-    def exit(self):
+    def exit(self):  # Exit program
         exit()
 
-    def basketManager(self):
+    def basketManager(self):  # Open basket manager
         self.master.withdraw()
         self.newWindow = Toplevel(self.master)
-        BasketInfos(self.newWindow, self.master)
+        BasketInfos(self.newWindow, self.master)v
