43ad4731d58a926d09a99f73b37965b2801cd033
wolfslayer <diegodisante22@gmail.com>
2025-04-22 23:07:59 +0200

    Update baskets.py

--- first_GUI_code/gest_cest_GUI/baskets.py
+++ first_GUI_code/gest_cest_GUI/baskets.py
@@ -1,7 +1,12 @@
+# Importing all contents from the module `Cestino`.
+# This likely includes the definition of the `Cestino` class used later in the code.
 from Cestino import *
 
-basket_1 = Cestino()
-basket_2 = Cestino()
-basket_3 = Cestino()
-basket_4 = Cestino()
-basket_5 = Cestino()
+# Creating multiple instances of the `Cestino` class.
+# These might represent different baskets initialized to hold items or perform specific tasks.
+
+basket_1 = Cestino()  # First basket instance
+basket_2 = Cestino()  # Second basket instance
+basket_3 = Cestino()  # Third basket instance
+basket_4 = Cestino()  # Fourth basket instance
+basket_5 = Cestino()  # Fifth basket instance
