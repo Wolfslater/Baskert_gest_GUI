aa7008aa63de830c3bdbea6591c8fcca1c30da95
wolfslayer <diegodisante22@gmail.com>
2025-04-26 13:33:23 +0200

    Create dropDown.py

---
+++ first_GUI_code/gest_cest_GUI/dropDown.py
@@ -0,0 +1,20 @@
+#Version 0.1.1 26/04/2025
+
+from tkinter import messagebox
+from tkinter.ttk import Combobox
+
+class DropDown:
+    def __init__(self, master, values, callback):
+        self.combobox = Combobox(master, width=40)
+        self.combobox["values"] = values
+        self.combobox["state"] = "readonly"
+        self.callback = callback
+
+        if self.callback:
+            self.combobox.bind("<<ComboboxSelected>>", self.callback)
+
+    def getBasket(self):
+        return self.combobox.get()
+
+    def showWarning(self, message="Please, select a basket first!"):
+        messagebox.showwarning("WARNING", message)
