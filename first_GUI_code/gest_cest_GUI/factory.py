cc673fb068d23543991f14aec82dbca30e8d1f2d
wolfslayer <diegodisante22@gmail.com>
2025-04-27 17:56:59 +0200

    Update factory.py

--- first_GUI_code/gest_cest_GUI/factory.py
+++ first_GUI_code/gest_cest_GUI/factory.py
@@ -1,23 +1,28 @@
-#version 0.0.1 26/04/2025
+#version 0.0.2 27/04/2025
 
 from tkinter import Label, Button, Entry
 
 class Factory:
+    @staticmethod
     def __init__(self, master):
         master = master
 
+    @staticmethod
     def newLabel(master, text=None):
         label = Label(master, text=text)
         return label
 
+    @staticmethod
     def newEntry(master, width=0):
         entry = Entry(master, width=width)
         return entry
 
+    @staticmethod
     def newButton(master, text=None, command=None):
         button = Button(master, text=text, command=command)
         return button
 
+    @staticmethod
     def gridConfig(widget, row, column, padx=0, pady=0, sticky="", columnspan=1):
         widget.grid(row=row, column=column, padx=padx, pady=pady, sticky=sticky, columnspan=columnspan)
         return widget
