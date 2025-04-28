ece57a4966e3481b4b7fc106082795b769a01d8e
wolfslayer <diegodisante22@gmail.com>
2025-04-28 18:52:28 +0200

    Update GUImanagement.py

--- first_GUI_code/gest_cest_GUI/GUImanagement.py
+++ first_GUI_code/gest_cest_GUI/GUImanagement.py
@@ -1,4 +1,4 @@
-# Version 0.3.5 26/04/2925
+n# Version 0.3.5 26/04/2925
 
 from Frutto import Frutto
 from addFruit import addFruit
@@ -11,16 +11,18 @@ class GUIManagement:
         self.master = master
         self.master.geometry("600x200")  # Set window size
         self.master.title("Main page")  # Set title
+        self.button = Factory.newButton
+        self.grid = Factory.gridConfig
 
         # Buttons with commands
-        self.addBtn = Factory.newButton(self.master, text="Add fruit to the basket.", command=self.add)
-        self.exitBtn = Factory.newButton(self.master, text="Exit the the program.", command=self.exit)
-        self.basketBtn = Factory.newButton(self.master, text="Open basket manager", command=self.basketManager)
+        self.addBtn = self.button(self.master, text="Add fruit to the basket.", command=self.add)
+        self.exitBtn = self.button(self.master, text="Exit the the program.", command=self.exit)
+        self.basketBtn = self.button(self.master, text="Open basket manager", command=self.basketManager)
 
         # Layout
-        Factory.gridConfig(widget=self.addBtn, row=0, column=0, padx=50)
-        Factory.gridConfig(widget=self.exitBtn, row=0, column=1, padx=15)
-        Factory.gridConfig(widget=self.basketBtn, row=0, column=2, padx=50)
+        self.grid(widget=self.addBtn, row=0, column=0, padx=50)
+        self.grid(widget=self.exitBtn, row=0, column=1, padx=15)
+        self.grid(widget=self.basketBtn, row=0, column=2, padx=50)
 
         self.fruit = Frutto()  # Initialize fruit object
 
